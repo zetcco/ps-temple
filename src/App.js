214e4797bf8d566b33fa87910ac5e4c57c33e88f
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2022-07-29 11:39:39 +0530

    Firebase config

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import { getAuth } from 'firebase/auth';
+import { db } from './firebase.config';
+
 function App() {
+  getAuth();
   return (
     <div className="text-3xl font-bold underline">Hello world!</div>
   );
---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwurIjOc88XPCZueJriD56R_bzEQqmsSk",
+  authDomain: "ps-temple-4d20b.firebaseapp.com",
+  projectId: "ps-temple-4d20b",
+  storageBucket: "ps-temple-4d20b.appspot.com",
+  messagingSenderId: "798697519435",
+  appId: "1:798697519435:web:1b999c340fe0d9696d90ce"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore();
