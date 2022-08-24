1446c6c9564b874af905391ed36479608fe18623
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2022-08-24 23:23:04 +0530

    Fixed Deletion confirm dialog modal

--- src/routes/TemplatePage.jsx
+++ src/routes/TemplatePage.jsx
@@ -54,7 +54,7 @@ function TemplatePage() {
                     <button className="btn btn-primary" onClick={() => window.open(template.gdriveLink, '_blank')}>Download</button>
                 </div>
                 <div className="absolute top-0 right-0 p-7">
-                    <a href="#modal-delete-confirm" className="text-neutral hover:text-error">
+                    <label htmlFor="modal-delete-confirm" className="text-neutral hover:text-error cursor-pointer">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                         <path className="st0" d="M19.6,7.2c0-1.6-1.3-2.8-2.8-2.8h-2.6V4.1c0-1.5-1.2-2.7-2.7-2.7S8.8,2.6,8.8,4.1v0.2H6.2
                             c-1.6,0-2.8,1.3-2.8,2.8c0,1.3,0.9,2.4,2.1,2.7v11.4c0,1.6,1.3,3,3,3h6.3c1.6,0,3-1.3,3-3V9.9C18.8,9.5,19.6,8.5,19.6,7.2z
@@ -62,16 +62,17 @@ function TemplatePage() {
                             M12.2,10v12.2h-1.4V10H12.2z M15.6,21.3c0,0.5-0.4,1-1,1h-0.5V10h1.4V21.3z M16.8,8H6.2C5.7,8,5.3,7.6,5.3,7.2s0.4-0.8,0.8-0.8h2.6
                             h5.4h2.6c0.5,0,0.8,0.4,0.8,0.8S17.3,8,16.8,8z"/>
                         </svg>
-                    </a>
+                    </label>
                 </div>
-
-                <div className="modal" id="modal-delete-confirm">
+                
+                <input type="checkbox" id="modal-delete-confirm" className="modal-toggle" />
+                <div className="modal">
                     <div className="modal-box">
                         <h3 className="font-bold text-lg">Confirm Delete</h3>
                         <p className="py-4">Are you sure you want to delete this template?</p>
                         <div className="modal-action">
-                            <a href="#" className="btn btn-outline btn-error">Cancel</a>
-                            <a href="#" className="btn btn-error" onClick={handleDelete}>Delete</a>
+                            <label htmlFor="modal-delete-confirm" className="btn btn-outline btn-error">Cancel</label>
+                            <label htmlFor="modal-delete-confirm" className="btn btn-error" onClick={handleDelete}>Delete</label>
                         </div>
                     </div>
                 </div>
